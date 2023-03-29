f2dc736482c16a1f17e381b1bff5d03177041f4e
Tommy-Sand <rosansandanasamy@gmail.com>
2023-03-28 19:10:19 -0600

    Finished

--- sockets.py
+++ sockets.py
@@ -21,10 +21,14 @@ from gevent import queue
 import time
 import json
 import os
+import sys
+import logging
 
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 class World:
     def __init__(self):
@@ -59,29 +63,71 @@ class World:
     def world(self):
         return self.space
 
-myWorld = World()        
+myWorld = World()
+
+clients = list()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    for client in clients:
+        #app.logger.debug(json.dumps({entity: entity, data: data}))
+        client.put(json.dumps({entity: data}))
 
+	
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    index = open('./static/index.html', 'r')
+    content = index.read()
+    index.close()
+    return content
 
-def read_ws(ws,client):
+def read_ws(ws, client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    while True:
+        try:
+            content = ws.receive() #Check websocket is open
+            if(not content):
+               continue
+            content = json.loads(content)
+            app.logger.debug(list(content.keys())[0])
+            key = list(content.keys())[0]
+            myWorld.set(key, content[key])
+        except Exception as e:
+            app.logger.debug(content)
+            app.logger.debug(e)
+            break
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    
+    client_queue = queue.Queue()
+    g = gevent.Greenlet.spawn(read_ws, ws, client_queue)
+    clients.append(client_queue)
+    init_world = myWorld.world()
+    
+    for i in init_world.keys():
+        ws.send(json.dumps({i: init_world[i]}))
+    while True:
+        try:
+            content = client_queue.get()
+            if(content):
+                if(content == "Closed"):
+                    app.logger.debug("pre closing")
+                    break
+                app.logger.debug("sending content")
+                ws.send(content)
+        except:
+            break
+    app.logger.debug("Closing connection")
+    g.kill()
+    clients.remove(client_queue)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,23 +145,42 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    if(request.method == 'PUT'):
+        data = flask_post_json()
+        if(data['X'] == None or data['y'] == None):
+            return 400
+        if(data['X'] < 0 or data['y'] < 0):
+            return 400
+        myWorld.set(entity, data)
+        return myWorld.get(entity)
+    elif(request.method == "POST"):
+        data = flask_post_json()
+        for key in data.keys():
+            myWorld.update(entity, key, data[key])
+        return myWorld.get(entity)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    if(request.method == 'GET'):
+        return myWorld.world()
+    elif(request.method == "POST"):
+        json = flask_post_json()
+        if(flask_post_json() != None):
+            for key in json.keys():
+                myWorld.set(key, json(key))
+        return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld.get(entity) 
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    return myWorld.world()
 
 
 
